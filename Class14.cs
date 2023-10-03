1a09ecbd044865b95d00fb95cf66c7582d8dd01e
Adusumilli Sri Ram Prasad <61102759+CodeNinja-194@users.noreply.github.com>
2023-10-03 14:58:27 +0530

    Explicit Consructor

---
+++ Class14.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Class14
+    {
+        public static void main(string[] args)
+        {
+            ExplicitConstructor obj1 = new ExplicitConstructor();
+            ExplicitConstructor obj2 = new ExplicitConstructor();
+            ExplicitConstructor obj3 = new ExplicitConstructor();
+            ExplicitConstructor obj4 = new ExplicitConstructor();
+            Console.ReadKey();
+        }
+    }
+    class ExplicitConstructor
+    {
+        public ExplicitConstructor()
+        {
+            Console.WriteLine("Explicit Constructor is Called!");
+        }
+    }
+}
